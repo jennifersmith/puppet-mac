6b9800482fe4a28cdee93d7af8b92dff0b4dc98e
Jennifer Smith <jennifer@home>
2011-12-03 21:11:22 -0600

    Basic alias for bashrc.. more to follow

--- modules/basic/manifests/init.pp
+++ modules/basic/manifests/init.pp
@@ -1,9 +1,16 @@
 # Bin folder from git
 class basic {
   notice("using user ${username}")
-      vcsrepo { "/Users/${username}/bin/":
+  $root_folder = "/Users/${username}"
+      vcsrepo { "${root_folder}/bin/":
         ensure => present,
         provider => git,
         source => "git@github.com:jennifersmith/misc.git"
- }
+  } 
+  # and it's aliases
+  file { "${root_folder}/.bashrc":
+    ensure => link,
+    target => "${root_folder}/bin/dotfiles/bashrc",
+  }
+
 }
