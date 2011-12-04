936b09165b69e34ea07d3b250806ddb7facdba66
Jennifer Smith <jennifer@home>
2011-12-03 21:30:17 -0600

    Added all the dotfile aliases in now...

--- modules/basic/manifests/init.pp
+++ modules/basic/manifests/init.pp
@@ -7,10 +7,29 @@ class basic {
         provider => git,
         source => "git@github.com:jennifersmith/misc.git"
   } 
-  # and it's aliases
-  file { "${root_folder}/.bashrc":
-    ensure => link,
-    target => "${root_folder}/bin/dotfiles/bashrc",
+  
+  define dotfile(){
+   file { "${root_folder}/.${name}":
+       ensure => link,
+           target => "${root_folder}/bin/dotfiles/${name}",
+    }
   }
 
+  define dotdir(){
+     file { "${root_folder}/.${name}":
+       ensure => link,
+           target => "${root_folder}/bin/dotfiles/${name}/",
+    }
+  }
+
+  class weirdassvim {
+   file { "${root_folder}/.vim":
+       ensure => link,
+           target => "${root_folder}/bin/dotfiles/vim/vim",
+    }
+  }
+
+  dotfile {["bash_profile", "bashrc" , "gemrc", "gitconfig", "gvimrc", "util", "vimrc"]:}
+  dotdir{"bash":}
+  class {'weirdassvim':}
 }
