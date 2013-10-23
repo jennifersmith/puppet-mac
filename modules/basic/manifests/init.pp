a836dca013cb9b856085ef7b32cb0dd3ba63cc75
Jennifer Smith <jennifer@home>
2013-10-23 15:46:34 +1100

    Few updates

--- modules/basic/manifests/init.pp
+++ modules/basic/manifests/init.pp
@@ -1,5 +1,7 @@
 class basic {               
  
+ Package { provider => "homebrew"}
+
  define github ($path = "${::user_homedir}dev/", $repo_name = $name, $github_user = jennifersmith, $ensure = present){
     vcsrepo { "${path}${name}/":
       ensure => $ensure,
@@ -11,24 +13,12 @@ class basic {
   github {"bin": path => $::user_homedir , repo_name=>misc, ensure=>latest}
 
   github {"private-settings": path =>"${::user_homedir}/bin/", require=>Github["bin"], ensure=>latest}
-  
-  #dev ones use the defaults apart from rapidftr 
-  github {["wire_tap", "4clojure_answers", "myblog", "plasma", "photo-management","flickr-clojure", "flickr-facebook-clj"]:}
-  
-  github {"rapidftr/dev": repo_name => RapidFTR}
-  github {"rapidftr/merge": repo_name => RapidFTR, github_user => jorgej}
+  github {"otfrom-org-emacs": path =>"${::user_homedir}/.emacs.d/", ensure=>latest}  
 
-  github {"mingle-stats" :}
-
+  github {["janus", "puppet", "myblog", "flocky", "janus-generators"]:}
+  
   # bbatsov emacs prelude
-  github {"emacs.d": github_user => bbatsov, repo_name => prelude, path => "${::user_homedir}/bin/dotfiles/", require=>Github["bin"], ensure=>latest}
-  # personal files symlinked in
-  file {"${::user_homedir}/bin/dotfiles/emacs.d/personal":
-    ensure => link,
-    target => "${::user_homedir}bin/dotfiles/prelude-personal",
-    require => Github["emacs.d"],
-    force => true
-  }
+
   define dotfile(){
    file { "${::user_homedir}.${name}":
        ensure => link,
@@ -51,7 +41,7 @@ class basic {
   }
 
   dotfile {["bash_profile", "bashrc" , "gemrc", "gitconfig", "gvimrc", "util", "vimrc"]:}
-  dotdir {["lein", "bash", "emacs.d"]:}
+  dotdir {["lein", "bash"]:}
   class {'weirdassvim':}
   class {'rvm::system': homedir=>$::user_homedir }  
 
@@ -61,20 +51,22 @@ class basic {
         '/usr/bin/ruby -e "$(curl -fsSL https://raw.github.com/gist/323731)"':
         creates => '/usr/local/bin/brew'}
  	
-  package {'emacs':
-    provider=>homebrew,
-    ensure=>HEAD,
-    install_options=> { flags => "--cocoa --use-git-head --HEAD"} }
-
   class{"leiningen": require => Dotdir["lein"]}
 
 
   package{ 'aspell' : provider=>homebrew, install_options =>{lang=>'eng'}}
-  package {'wget': provider=>homebrew}
-  package {'ack': provider=>homebrew}
   package {'llvm' : provider=>homebrew, install_options=>{flags=>'--universal'}}
 
-package { 'gist' : provider=>homebrew}
+package { ["ack",
+"gist",
+"git",
+"glib",
+"gnupg",
+"postgresql",
+"python",
+"readline",
+"s3cmd",
+"wget"] :}
 
 }
 
